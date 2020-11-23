1c75b8be7f73c4d63cc1ed24a25b4054846e2b8b
Sunil Kumar <sunilkanveri@gmail..com>
2020-11-23 16:51:20 -0500

    commit

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -7,6 +7,17 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+            Console.ReadKey(true);
+            if (true == true)
+            {
+                Console.Write("\nPress any key to exit...");
+            }
         }
+
     }
 }
